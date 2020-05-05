988589f06b089962571f208b53238470d7df8cd1
zdraffko <zdravkomb1@gmail.com>
2020-05-05 16:09:38 +0300

    Updates

---
+++ precache-manifest.5f7853abb2bebdfdc9e089188f1ac1cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8ae58709f14af13279fd93ca4ac77b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06a990e073beccd7a005",
+    "url": "/portfolio/static/css/main.e2020084.chunk.css"
+  },
+  {
+    "revision": "8dec37475b9eee49f62c",
+    "url": "/portfolio/static/js/2.d58dffc9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.d58dffc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a990e073beccd7a005",
+    "url": "/portfolio/static/js/main.afa02a48.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
